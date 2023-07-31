cbedfc2f86e8354b674173f14177c4ed90a14be2
Luiz Costa <luizcosta@redhat.com>
2023-07-30 21:32:32 -0300

    build WIP

--- src/cli_builder.py
+++ src/cli_builder.py
@@ -1,5 +1,6 @@
 import click
 import helpers
+import os
 
 
 _global = helpers.get_global()
@@ -13,9 +14,48 @@ logger = helpers.get_logger()
     default="./cache/cachito_repo",
     help="Path where the Cachito repository is located",
 )
-def cmd_build(clone_path):
+# option: --file, -f
+@click.option(
+    "--file",
+    "-f",
+    default="./Containerfile",
+    help="Path to the Containerfile to build",
+)
+# option: --build-context
+@click.option(
+    "--build-context",
+    "-c",
+    help="Path to the build context. Defaults to the directory of the Containerfile",
+)
+def cmd_build(clone_path, file, build_context):
     """Build a container image using Cachito servers"""
-    print("hello")
+
+    # Check if Containerfile exists
+    if not os.path.isfile(file):
+        logger.error("Containerfile not found: " + file)
+        exit(1)
+
+    services = helpers.get_services(clone_path)
+
+    file_abs = os.path.abspath(file)
+    if build_context:
+        build_context_abs = os.path.abspath(build_context)
+    else:
+        build_context_abs = os.path.dirname(file_abs)
+
+    # TODO add interceptor
+    # TODO add env file creation
+    # TODO add limited DNS resolution
+
+    cmd_out = helpers.run(["podman", "build", "-f", file_abs, "-t", "test", build_context_abs])
+    if cmd_out.returncode != 0:
+        logger.error("Error building container image")
+        exit(1)
+    print(cmd_out.stdout.decode("utf-8"))
+
+
+
+
 
 
 # Click
